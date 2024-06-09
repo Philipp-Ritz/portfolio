6042282714187384c1e6c8298dc64fb5fe7d2684
Philipp-Ritz <philipp.ritz1998@web.de>
2024-06-09 22:00:19 +0200

    Add files via upload

---
+++ repos/kurs c sharp anfänger/kurs c sharp/Program.cs
@@ -0,0 +1,49 @@
+﻿string meinname = "philipp";
+Console.WriteLine(meinname);
+
+int ganzezahlen = 4;
+ganzezahlen++;
+Console.WriteLine(ganzezahlen);
+
+void zählbis3()
+{
+    Console.WriteLine(1);
+    Console.WriteLine(2);
+    Console.WriteLine(3);
+    Console.WriteLine("----");
+
+
+}
+
+zählbis3();
+zählbis3();
+zählbis3();
+
+void saghallo(string vorname)
+{
+    Console.WriteLine("moin " + vorname);
+}
+saghallo("philipp");
+
+float punktvorstrichrechnung(float number)
+{
+    float resultat = number + number * 2;
+    return resultat;
+}
+float punktvorstrich = punktvorstrichrechnung(5);
+Console.WriteLine(punktvorstrich);
+
+
+double umfangeineskreises(double radius)
+{
+    double result = 2 * Math.PI * radius;
+    return result;
+}
+
+Console.WriteLine(umfangeineskreises(5));
+
+
+string eingabe = Console.ReadLine();
+Console.WriteLine(eingabe + "1");
+
+
